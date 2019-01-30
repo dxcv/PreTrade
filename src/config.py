4c2b2629f0705787e61ac6aade8e5238d86ad78a
zoujunlin <1927007992@qq.com>
2019-01-30 17:50:05 +0800

    3实5虚改为3实6虚

--- src/config.py
+++ src/config.py
@@ -7,30 +7,30 @@ import numpy as np
 from utils.InfoApi import  *
 
 sql="""
-SELECT TOP (2) 
-	  TradingDay,
+SELECT TOP (5) 
+      TradingDay,
 	  InstrumentID,
       [SettlementPrice]
-  FROM [PreTrade].[dbo].[SettlementDetail] where InstrumentID='CF905' or InstrumentID='CF909'  order by TradingDay desc
+  FROM [PreTrade].[dbo].[SettlementDetail] where InstrumentID='CF905' or InstrumentID='CF909' or InstrumentID='CF911'  or InstrumentID='CF903'  or  InstrumentID='CF907'  order by TradingDay desc
 """
 def GetInstrument():
     pass
 
 
 if __name__=='__main__':
+    """ =====connect mysql for presettlement data if you can otherwise fill in the data at the end yourself======"""
     PreSettle = {}
     info=InfoApi()
     resList = info.mysql.ExecQuery(sql)
-    for i in resList:
+    PreSettle=dict( (str(name).encode("utf-8"),int(value)) for TradingDay,name,value in resList)
 
-        PreSettle[str(i[1]).encode("utf-8")] = int(float(i[2]))
     print PreSettle
     for key in PreSettle.keys():
         atmstrike=round(PreSettle[key]/1000.0*5)*200
-        for k in np.arange(atmstrike-600, atmstrike+1400, 200):
+        for k in np.arange(atmstrike-600, atmstrike+1600, 200):
             symbol=key+'C'+str(int(k) )
             print symbol
-        for k in np.arange(atmstrike-1200, atmstrike+800, 200):
+        for k in np.arange(atmstrike-1200, atmstrike+1000, 200):
             symbol=key+'P'+str(int(k))
             print symbol
     GetInstrument()
