6764e163160a364ba3ee0139d6b97446e52a137e
zoujunlin <1927007992@qq.com>
2019-02-19 14:39:26 +0800

    level1数据行情处理

--- src/MainCleanData/Main.py
+++ src/MainCleanData/Main.py
@@ -5,17 +5,26 @@
 """主力合约数据处理脚本"""
 from utils.InfoApi import *
 import datetime
+from utils.TradingDay import  NextTradingDay
+from Level1Clean import *
 
+def Getdirector(info):
+    if info.cleanDatadict[1] in info.setting.level5:
+        return "T:/level_5/"+info.cleanDatadict[0]+"/","DC"+info.cleanDatadict[2],5
+    else:
+        return "T:/level_1/"+info.cleanDatadict[0]+"/",info.cleanDatadict[2],1
 
-def CleanData(MainInstrument,info):
-    pass
-
+def CleanData(info):
+    directory,filename,type=Getdirector(info)
+    print directory,filename
+    if type==1:
+        Level_1_clean(filename, directory, info)
 
 if __name__=='__main__':
 
     ProductCodeList=['ni','au','ag','rb','i','m','TA']
     StartDay="20180801"
-
+    storeDirectory="D:/DATA"
 
     """Read SqlServer Get Main InstrumentID by ProductCode"""
     pass
@@ -23,14 +32,15 @@ if __name__=='__main__':
     info = InfoApi()
     info.GetDbHistoryConnect()
 
-    t = TradingDay(info)
+    t = NextTradingDay.TradingDay(info)
     startdate = datetime.datetime.strptime(StartDay, "%Y%m%d")
-    enddate = datetime.datetime.now()
+    enddate = datetime.datetime.now()-datetime.timedelta(days=1)
 
     while startdate.strftime("%Y%m%d") <= enddate.strftime("%Y%m%d"):
         print startdate
         MainInstrument=info.GetMainInstrumentIdByProductCode(ProductCodeList[0],startdate.strftime("%Y-%m-%d"))
-        CleanData(MainInstrument,info)
+        info.cleanDatadict = [startdate.strftime("%Y%m%d"), ProductCodeList[0],MainInstrument,storeDirectory]
+        CleanData(info)
         startdate = t.NextTradingDay(startdate.strftime("%Y%m%d"), True)
         startdate = datetime.datetime.strptime(startdate, "%Y%m%d")
 
