a3f148c9a17b48a0d7a2f8100e399a8ec903aa09
zoujunlin <1927007992@qq.com>
2019-02-23 15:22:56 +0800

    阶段统计排名

--- src/GetCompanyPosition/PositionAPI.py
+++ src/GetCompanyPosition/PositionAPI.py
@@ -3,7 +3,7 @@
 # @Author  : ZouJunLin
 """爬取持仓共用的API"""
 import re
-import csv,os,codecs
+import csv,os,codecs,datetime
 
 def GetDCEPosition(info,TradingDay,ExchangeID):
 
@@ -87,11 +87,52 @@ def GetDCEStagePosition(info):
     ListDataToExcel(listdata,filename)
 
 
+def GetDCEStagedStatistic(info,TradingDay,ExchangeID):
+    """阶段性统计排名"""
+    beginmonth = TradingDay.strftime("%Y%m")
+    url = "http://www.dce.com.cn/publicweb/quotesdata/varietyMonthYearStatCh.html?"
+    url = url + "varietyMonthYearStatQuotes.trade_type=0&varietyMonthYearStatQuotes.begin_month=%s"
+    url = url%beginmonth
+    GetDCEStatistic(info,url,ExchangeID,beginmonth)
+
+def  GetDCEStatistic(info,url,ExchangeID,beginmonth):
+    header = {
+        'Connection': 'keep-alive',
+        'Cache-Control': 'no-cache',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Cookie': 'JSESSIONID=1311697EA3395C8127FD0BB9D51B1742; WMONID=j1TJsMZrARA; Hm_lvt_a50228174de2a93aee654389576b60fb=1550114491,1550801897,1550826990,1550890979; Hm_lpvt_a50228174de2a93aee654389576b60fb=1550892897',
+        'Host': 'www.dce.com.cn',
+        'Pragma': 'no-cache',
+        'Upgrade-Insecure-Requests': '1'
+    }
+    html = info.mysplider.getUrlcontent(url, header=header)
+    listdata = info.mysplider.tableTolistByNum(html, "DCE", 0)
+    print url
+    """write to xls"""
+    ext = '.csv'
+    parent = "D:/GitData/StagePosition/"
+    if not os.path.exists(parent + ExchangeID):
+        os.mkdir(parent + ExchangeID)
+    if not os.path.exists(parent + ExchangeID + "/" + beginmonth):
+        os.mkdir(parent + ExchangeID + "/" + beginmonth)
+    filename = beginmonth+"月度统计表"+ext
+    filename = parent + ExchangeID  + "/"+beginmonth+"/" + filename
+    col=[]
+    col0=listdata[0]
+    col1=listdata[1]
+    col=[col0[0],col1[0]+col0[1],col1[1],col1[2],col1[3]+col0[1],col1[4],col1[5]+col0[2],col1[6],col1[7],col1[8]+col0[2],col1[9],col1[10],col1[11],col1[12]]
+    listdata=listdata[1:]
+    listdata[0]=col
+    ListDataToExcel(listdata, filename)
+
+
 def ListDataToExcel(listdata,filename):
     """a public method  that list data write ext extension file"""
 
     # file_backup=f = codecs.open(parent+info.QryPositionExchangeID+"/"+filename,'wb','utf-8')
-    csvfile = file(filename, 'wb')
+    csvfile = file(filename.decode("utf-8"), 'wb')
     csvfile.write(codecs.BOM_UTF8)
     writer=csv.writer(csvfile)
     writer.writerows(listdata)
---
+++ src/GetCompanyPosition/StageStatistic.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/2/22 16:43
+# @Author  : ZouJunLin
+"""获取四大交易所阶段性统计排名"""
+
+import sys
+import os,datetime
+dir_path=os.path.dirname(os.path.abspath(".."))
+sys.path.append(dir_path)
+from utils.InfoApi import *
+from utils.Mysplider import *
+from PositionAPI import *
+from utils.TradingDay import *
+
+
+
+def main(startdate,info):
+    """
+    获取四大交易所阶段性统计排名
+    :return:
+    """
+
+    GetDCEStagedStatistic(info,startdate,"DCE")      #阶段性统计
+
+
+
+
+if __name__=="__main__":
+    info=InfoApi()
+    info.GetDbHistoryConnect()
+    info.Get_Msplider()
+
+    t = TradingDay(info)
+    startdate = datetime.datetime.strptime("20190222", "%Y%m%d")
+    startdate=info.mydate.GetCurrentB(startdate)
+    enddate = datetime.datetime.now()
+    enddate=info.mydate.GetNextmonthB(enddate)
+    mysplider = info.mysplider
+
+    while startdate.strftime("%Y%m%d") <enddate.strftime("%Y%m%d"):
+        print startdate
+        main(startdate,info)
+        startdate = info.mydate.GetNextmonthB(startdate)
+
+    info.mysql.Disconnect()
