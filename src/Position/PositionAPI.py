4503613ac1b2c18a6b36ba0326ea571ab498dfdc
zoujunlin <1927007992@qq.com>
2019-04-26 17:46:15 +0800

    压缩zip文件方法

--- src/Position/PositionAPI.py
+++ src/Position/PositionAPI.py
@@ -313,11 +313,16 @@ def zipDir(dirpath,outFullName):
     :param outFullName: 压缩文件保存路径+xxxx.zip
     :return: 无
     """
-    zip = zipfile.ZipFile(outFullName,"w",zipfile.ZIP_DEFLATED)
-    for path,dirnames,filenames in os.walk(dirpath):
-        # 去掉目标跟路径，只对目标文件夹下边的文件及文件夹进行压缩
-        fpath = path.replace(dirpath,'')
-
+    # zip = zipfile.ZipFile(outFullName,"w",zipfile.ZIP_DEFLATED)
+    # for path,dirnames,filenames in os.walk(dirpath):
+    #     # 去掉目标跟路径，只对目标文件夹下边的文件及文件夹进行压缩
+    #     fpath = path.replace(dirpath,'')
+    #
+    #     for filename in filenames:
+    #         zip.write(os.path.join(path,filename),os.path.join(fpath,filename))
+    # zip.close()
+    zip = zipfile.ZipFile(outFullName, 'w', zipfile.ZIP_DEFLATED)
+    for dirpath, dirnames, filenames in os.walk(dirpath):
         for filename in filenames:
-            zip.write(os.path.join(path,filename),os.path.join(fpath,filename))
+            zip.write(os.path.join(dirpath, filename))
     zip.close()
